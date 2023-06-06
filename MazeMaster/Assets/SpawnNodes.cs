0cd83b352110720ca6d62dc2c11ef512406b879b
SharmaAbhishek28 <135024126+SharmaAbhishek28@users.noreply.github.com>
2023-06-06 23:09:50 +0530

    bug fixed

--- MazeMaster/Assets/SpawnNodes.cs
+++ MazeMaster/Assets/SpawnNodes.cs
@@ -4,23 +4,10 @@ using UnityEngine;
 
 public class SpawnNodes : MonoBehaviour
 {
-    int numToSpawn = 27;
-
-    public float currentSpawnOffset;
-    public float spawnOffset = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
-        if (gameObject.name == "Node")
-        {
-            currentSpawnOffset = spawnOffset;
-            for (int i = 0; i < numToSpawn; i++)
-            {
-                //Clone a new Node
-                GameObject clone = Instantiate(gameObject, new Vector3(transform.position.x + currentSpawnOffset, transform.position.y, 0),Quaternion.identity);
-                currentSpawnOffset += spawnOffset;
-            }
-        }
+        
     }
 
     // Update is called once per frame
