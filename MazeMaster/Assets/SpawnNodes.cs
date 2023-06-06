2828f5d84a8ea60907b6aed97145b99e1da0ffe1
SharmaAbhishek28 <135024126+SharmaAbhishek28@users.noreply.github.com>
2023-06-07 00:32:38 +0530

    Create: Nodes chart

--- MazeMaster/Assets/SpawnNodes.cs
+++ MazeMaster/Assets/SpawnNodes.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class SpawnNodes : MonoBehaviour
 {
+    int numToSpawn = 28;
+
+    public float currentSpawnOffset;
+    public float spawnOffset = 0.3f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (gameObject.name == "Node")
+        {
+            currentSpawnOffset = spawnOffset;
+            for (int i = 0; i <  numToSpawn; i++)
+            {
+                // clone the node
+                GameObject clone = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y + currentSpawnOffset, 0), Quaternion.identity);
+                currentSpawnOffset += spawnOffset;
+            }
+        }
     }
 
     // Update is called once per frame
